93a6e6021b4cd44c0ccae68d85e6956ed6254ebc
Jesie Oldenburg <kitglo46@gmail.com>
2020-03-26 18:24:58 -0500

    Components are now filtered by user

--- komponentkeeperapp/views/components/list.py
+++ komponentkeeperapp/views/components/list.py
@@ -1,25 +1,30 @@
 import sqlite3
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
-from komponentkeeperapp.models import Component
+from komponentkeeperapp.models import Component, Creator
 
 @login_required
 def components_list(request, pk=None):
     # Check if the request made is a GET request
+    
+    # get a reference to the logged in user 
+    creatorId = Creator.objects.get(user_id=request.user.id)
+    print('Creator ID', creatorId)
     if request.method == 'GET':
         pk = None
         # Assign all of the requested items to a variable
-        all_components = Component.objects.all()
-        
+        # * all_components = Component.objects.all() 
+        user_components = Component.objects.filter(creator=creatorId.id)
         # Then store each of the requested resource's values in a variable named after the keys in the model
         name = request.GET.get('name')
-        creator = request.GET.get('creator')
+        # creator = request.GET.get('creator')
+        # component.creator_id = creatorId.id
         image = request.GET.get('image')
         description = request.GET.get('description')
         
         template = 'components/list.html'
         context = {
-            'all_components': all_components
+            'user_components': user_components # * Changed
         }
 
         # return the render method passing in request, template and context as params
@@ -39,34 +44,3 @@ def components_list(request, pk=None):
     #     print(new_component.name)
     #     new_component.save()
     #     return redirect(reverse('komponentkeeperapp:components'))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# def component_image_upload(request):
-#     # Post an image to the database
-#     if request.method == 'POST':
-#         # Create a new instance of the component form and pass the posted values, including the files to it.
-#         form = AddComponentForm(request.POST, request.FILES)
-        
-#         # If the form fields are validated
-#         if form.is_valid(): 
-#             # Save the form to the db
-#             form.save() 
-#             # And redirect the user to the landing
-#             return redirect('success') 
-        
-#     else:
-#         form = AddComponentForm()
-#         pass
-#     return render(request, 'components:form.html', {'form': form})
-
-
-# def success(request): 
-#     return HttpResponse('successfully uploaded') 
